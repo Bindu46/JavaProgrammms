02a9869e1523876ae81152e5e06e25bfac628355
Bindu46 <b74265601@gmail.com>
2026-02-17 16:37:45 +0530

    Add files via upload

---
+++ InstanceVariables.java
@@ -0,0 +1,9 @@
+class InstanceVariables
+{
+int a=10;
+  public static void main(String args[])
+{
+InstanceVariables obj=new InstanceVariables();
+System.out.println("instance variables"+obj.a);
+}
+}
