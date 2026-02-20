fde495740fa26b3cfe28ebced5bbb6c018f1d79f
Bindu46 <b74265601@gmail.com>
2026-02-20 17:44:03 +0530

    Add files via upload

---
+++ Bindu.java
@@ -0,0 +1,17 @@
+class Bindu
+{
+  int id;
+  string name;
+ Bindu(int i,name n)
+{
+id=i;
+name =n;
+}
+public static void main(String args[])
+{
+ Bindu s1=new Bindu(12,"Jagan");
+ System.out.println(s1.id);
+System.out.println(s1.name);
+}
+
+}
